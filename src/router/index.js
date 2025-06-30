d1ab0182af56a0672917fdec8b4ff6866eb79baf
jesi-nancy <josephjesinanancy@gmail.com>
2025-06-30 01:52:54 -0400

    Initial setup for deployment

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 // Component Imports
 import LogIn from '@/components/LogIn.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
